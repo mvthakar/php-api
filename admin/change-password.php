dc52a2effb3c8ecd636538118226eff7811aadd7
Meet Thakar <mvthakar@outlook.com>
2024-01-13 09:03:50 +0530

    Add breadcrumbs

--- admin/change-password.php
+++ admin/change-password.php
@@ -7,7 +7,9 @@
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item">Settings</li>
+            <li class="breadcrumb-item active">Change password</li>
           </ol>
         </div>
       </div>
