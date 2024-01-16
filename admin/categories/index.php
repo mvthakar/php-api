8c0248700a00e2e563ef881ba4b1ed59b8f0a280
Meet Thakar <mvthakar@outlook.com>
2024-01-16 13:42:07 +0530

    Implement category list and delete

--- admin/categories/index.php
+++ admin/categories/index.php
@@ -1,4 +1,3 @@
-<?php Authorize::forRoles(["Admin"]);?>
 <div class="content-wrapper">
   <div class="container">
     <section class="content-header">
@@ -29,13 +28,13 @@
             <table id="items" class="table table-striped item-list">
               <thead>
                 <tr>
-                  <th scope="col">Number</th>
-                  <th scope="col">Image</th>
-                  <th scope="col">Name</th>
-                  <th scope="col">Actions</th>
+                  <th width="10%" scope="col" class="text-center">Number</th>
+                  <th width="10%" scope="col" class="text-center d-sm-none d-md-table-cell d-none d-sm-table-cell">Image</th>
+                  <th width="" scope="col">Name</th>
+                  <th width="20%" scope="col">Actions</th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody id="categories">
                 <tr>
                   <td class="py-5 text-center" colspan="4">
                     <div class="spinner-border" role="status">
@@ -52,4 +51,18 @@
     </section>
   </div>
 </div>
+<div class="position-fixed bottom-0 right-0 p-3" style="z-index: 5; right: 0; bottom: 0;">
+  <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
+    <div class="toast-header bg-danger">
+      <strong class="mr-auto">Error</strong>
+      <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="toast-body bg-danger" id="error-message">
+      
+    </div>
+  </div>
+</div>
+
 <script src="<?=urlOf('js/categories.js')?>"></script>
--- admin/js/categories.js
+++ admin/js/categories.js
@@ -10,13 +10,76 @@ async function deleteItem()
     if (id == null)
         return;
 
-    await get(`categories/delete.php?id=${id}`);
+    let response = await get(`categories/delete.php?id=${id}`);
+    if (response.status == 200)
+    {
+        window.location.reload();
+        return;
+    }
+
+    $('#error-message').text(response.message);
+    $('.modal').modal('hide');
+    $('.toast').toast('show');
 }
 
 async function loadItems()
 {
     let response = await get('categories/get.php');
-    // ...
+    let rows = "";
+
+    response.message.forEach((category, index) => {
+        rows += `
+            <tr>
+                <td class="text-center">${index + 1}</td>
+                <td class="text-center d-sm-none d-sm-none d-md-table-cell d-none d-sm-table-cell"><img width="50px" src="${uploads(`categories/${category.imageFileName ?? 'default.jpg'}`)}" /></td>
+                <td>${category.name}</td>
+                <td>
+                    <div class="btn-group d-none d-lg-flex" role="group" aria-label="Actions">
+                        <a role="button" class="btn btn-primary" href="${admin(`categories/edit.php?id=${category.slug}`)}" data-toggle="tooltip" data-placement="bottom" title="Edit">
+                            <i class="far fa-edit"></i>
+                        </a>
+                        <a role="button" class="btn btn-danger" onclick="showDeleteConfirmation('${category.slug}')" data-toggle="tooltip" data-placement="bottom" title="Delete">
+                            <i class="far fa-trash-alt"></i>
+                        </a>
+                    </div>
+                    <div class="btn-group-vertical d-lg-none" role="group" aria-label="Actions">
+                        <a role="button" class="btn btn-primary" href="${admin(`categories/edit.php?id=${category.slug}`)}"
+                        data-toggle="tooltip" data-placement="bottom" title="Edit">
+                        <i class="far fa-edit"></i>
+                        </a>
+                        <a role="button" class="btn btn-danger"
+                        onclick="showDeleteConfirmation('${category.slug}')" data-toggle="tooltip"
+                        data-placement="bottom" title="Delete">
+                        <i class="far fa-trash-alt"></i>
+                        </a>
+                    </div>
+                    <div class="modal fade" id="modal-delete" tabindex="-1" role="dialog"
+                        aria-labelledby="modal-delete-title" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h5 class="modal-title" id="modal-delete-title">Cofirmation</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            </div>
+                            <div class="modal-body">
+                                Are you sure you want to delete this item?
+                            </div>
+                            <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                            <button id="btn-yes" type="button" class="btn btn-danger"
+                                onclick="deleteItem()">Yes</button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            `;
+    });
+
+    $('#categories').html(rows);
 }
 
 $(loadItems);
